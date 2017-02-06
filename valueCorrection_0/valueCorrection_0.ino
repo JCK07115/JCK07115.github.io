8fe1e70a19ac46f224b18d109b0340d7a1c43384
JCK07115 <JCK07115@users.noreply.github.com>
2017-02-05 23:29:51 -0500

    accelerometer_value_correction
    trying to correct the errors in the accelerometer readings, due vibrations over a short period of time, using a "fixed-value" matrix. this will probably not work for all accel. values, and a filter (Kalman) might be implemented to account for variations in the errors. accel. readings suffer short-term vibrations that cause readings over a differential to be off, as opposed to gyrsocopes that are usually accurate over short durations but eventually drift with time.

---
+++ valueCorrection_0/valueCorrection_0.ino
@@ -0,0 +1,129 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS0.h>
+#include <MatrixMath.h>
+
+#define N (3)
+
+float A[N][N];  //correction array
+float B[N][1];  //raw accel. values
+float C[N][1];  //corrected values, hopefully
+sensors_event_t accel, mag, gyro, temp; //sensor_event that will initially store readings
+
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);
+
+void configureSensor(void)
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
+  
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t accel, mag, gyro, temp;
+  
+  lsm.getSensor(&accel, &mag, &gyro, &temp);
+  
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(accel.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(accel.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(accel.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(accel.max_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Min Value:    ")); Serial.print(accel.min_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Resolution:   ")); Serial.print(accel.resolution); Serial.println(F(" m/s^2"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(mag.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(mag.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(mag.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(mag.max_value); Serial.println(F(" uT"));
+  Serial.print  (F("Min Value:    ")); Serial.print(mag.min_value); Serial.println(F(" uT"));
+  Serial.print  (F("Resolution:   ")); Serial.print(mag.resolution); Serial.println(F(" uT"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(gyro.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(gyro.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(gyro.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(gyro.max_value); Serial.println(F(" rad/s"));
+  Serial.print  (F("Min Value:    ")); Serial.print(gyro.min_value); Serial.println(F(" rad/s"));
+  Serial.print  (F("Resolution:   ")); Serial.print(gyro.resolution); Serial.println(F(" rad/s"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(temp.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(temp.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(temp.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(temp.max_value); Serial.println(F(" C"));
+  Serial.print  (F("Min Value:    ")); Serial.print(temp.min_value); Serial.println(F(" C"));
+  Serial.print  (F("Resolution:   ")); Serial.print(temp.resolution); Serial.println(F(" C"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  
+  delay(500);
+}
+
+void setup(void) {
+  
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+  Serial.begin(9600);
+
+  if(!lsm.begin()) {
+
+    //there was a problem detecting the sensor, re-check wiring
+    Serial.print(F("Ooops, no LSM9DS0 detected ... Check your wiring or I2C ADDR!"));
+    while(1);
+  }
+  Serial.println(F("Found LSM9DS0 9DOF"));
+  
+  //displaySensorDetails();
+
+  //set up the sensor gain and integration time;
+  configureSensor();
+
+  Serial.println("");
+}
+
+void loop(void) {
+
+//lsm.getEvent(&accel, &mag, &gryo, &temp);
+
+  lsm.readAccel();
+  Serial.println(lsm.accelData.x);
+  Serial.println(lsm.accelData.x);
+  Serial.println(lsm.accelData.x);
+
+  A[0][0] = 1.1070; A[0][1] = -0.0255; A[0][2] = -0.0729;
+  A[1][0] = 0.1121; A[1][1] =  0.9867; A[1][2] = -0.0620;
+  A[2][0] = 0.1580; A[2][1] = -0.1240; A[2][2] =  1.0590;
+  
+  B[0][0] = lsm.accelData.x;
+  B[1][0] = lsm.accelData.y;
+  B[2][0] = lsm.accelData.z;
+
+  Matrix.Multiply((float*) A, (float*) B, N, N, 1, (float*) C);
+  Matrix.Print((float*) C, N, 1, "C");
+  delay(1000);
+}
